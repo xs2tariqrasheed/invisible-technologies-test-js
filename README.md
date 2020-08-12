4afbfdb185f162795233411bc797fc1422a0da59
xs2tariqrasheed <xs2tariqrasheed@gmail.com>
2020-08-12 14:21:53 +0500

    try catch block added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 1- Clone this repo.
 2- Make sure you have node 10.15.3 or later.
-3- run `npm install`
-4- run `npm run start`
+3- Run `npm install`
+4- Run `npm run start`
 
 you'll the result in console
--- index.js
+++ index.js
@@ -6,20 +6,32 @@ const locations = [
   "Brazil, Curitiba",
 ];
 
+const WEATHER_API_KEY = "f1d2dfe8f5b5e69350af644692a6b641";
+const TIME_API_KEY = "qTXJkenGWqWPEdUi5d9zaeNdsvbFHf";
+
 async function printTimeAndWeather(locations) {
   for (location of locations) {
-    const weatherResponse = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f1d2dfe8f5b5e69350af644692a6b641`
-    );
-    // we have only 5 free call of this api
-    const timeResponse = await axios.get(
-      `https://www.amdoren.com/api/timezone.php?api_key=qTXJkenGWqWPEdUi5d9zaeNdsvbFHf&loc=${location}`
-    );
+    try {
+      const weatherResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${WEATHER_API_KEY}`
+      );
+      // we have only 5 free call of this api
+      const timeResponse = await axios.get(
+        `https://www.amdoren.com/api/timezone.php?api_key=${TIME_API_KEY}&loc=${location}`
+      );
 
-    const { weather } = weatherResponse.data;
-    console.log(
-      `${location}(Weather): ${weather[0].main} ${weather[0].description}, Time: ${timeResponse.data.time}`
-    );
+      const { weather } = weatherResponse.data;
+      console.log(
+        `${location}(Weather): ${weather[0].main} ${weather[0].description}, Time: ${timeResponse.data.time}`
+      );
+    } catch (error) {
+      console.log("Error: ", error);
+      if (error.message) {
+        console.log("Error Message: ", error.message);
+      } else {
+        console.log("Error: Something went wrong.");
+      }
+    }
   }
 }
 
