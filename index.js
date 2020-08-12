2ba73aa3d89e6eecbded80b0c4d83ff649d498e2
xs2tariqrasheed <xs2tariqrasheed@gmail.com>
2020-08-12 14:13:42 +0500

    completed

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const axios = require("axios").default;
 
-async function foo() {
-  for (location of [1, 2, 3]) {
-    console.log(location);
-    const locationRes = await axios.get(
-      "https://api.openweathermap.org/data/2.5/weather?q=pakistan&appid=f1d2dfe8f5b5e69350af644692a6b641"
+const locations = [
+  "Pakistan, Lahore",
+  "Afghanistan, Kabul",
+  "Brazil, Curitiba",
+];
+
+async function printTimeAndWeather(locations) {
+  for (location of locations) {
+    const weatherResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f1d2dfe8f5b5e69350af644692a6b641`
     );
-    const timeRes = await axios.get(
-      "https://www.amdoren.com/api/timezone.php?api_key=WTgxAsJVA74faYS7cspnxHYCmaMxNw&loc=Pakistan,%20Lahore"
+    // we have only 8 free call of this api
+    const timeResponse = await axios.get(
+      `https://www.amdoren.com/api/timezone.php?api_key=qTXJkenGWqWPEdUi5d9zaeNdsvbFHf&loc=${location}`
     );
-    console.log(timeRes.data);
+
+    const { weather } = weatherResponse.data;
+    console.log(
+      `${location}(Weather): ${weather[0].main} ${weather[0].description}, Time: ${timeResponse.data.time}`
+    );
   }
 }
 
-foo();
+printTimeAndWeather(locations);
